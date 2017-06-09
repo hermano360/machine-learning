d0833085c7db6e26e0359ec6b1912752ac3e4d21
Herminio Garcia <hermano360@gmail.com>
2017-06-09 15:23:30 -0700

    init commit

---
+++ Part_1_-_Data_Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  4 12:33:56 2017
+
+@author: herminiogarcia
+"""
+
+# Data Preprocessing
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,3].values
+
+
+
+# Splitting the dataset into Training set and Test set
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2,random_state=0)
+
+
+# Feature Scaling
+"""
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+"""
